bae5227544ce160992a1bcc48330b185934f69fe
Ian Travers <mr.itpe@gmail.com>
2020-09-30 16:16:15 +0300

    Translation for verify email (delete duplicate alert message)

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,30 +1,28 @@
 <x-layout-auth>
-    @if (session('resent'))
-        <div class="alert alert-success" role="alert">
-            Новое письмо с инструкцией по верификации отправлено на ваш адрес email.
-        </div>
-    @endif
-    <div class="card w-50 mx-auto">
-        <div class="card-header">
-            <h2 class="text-muted">Верификация адреса email</h2>
-        </div>
-        <div class="card-body">
+    <div class="col-7 mx-auto">
+        <div class="card">
+            <div class="card-header">
+                <h2 class="text-muted">Верификация адреса email</h2>
+            </div>
             @if (session('resent'))
-                <div class="alert alert-success" role="alert">
+                <div class="alert alert-success rounded-0" role="alert">
                     Новое письмо с инструкцией по верификации отправлено на ваш адрес email.
                 </div>
             @endif
-            На адрес email, указанный при регистрации, выслано письмо для подтверждения вашего адреса email. Это
-            процедура необходима для дальнейшей работе в системе. В настройках вашего профиля можно указать
-            рассылку уведомлений на события, происходящие в системе, которая будет посылаться на ваш адрес
-            email. Если вы не получили email, то
-            <form class="d-inline" method="post" action="{{ route('verification.resend') }}">
-                @csrf
-                <button type="submit"
-                        class="btn btn-link p-0 m-0 align-baseline">кликните сюда и система отправит вам новый
-                </button>
-                .
-            </form>
+            <div class="card-body">
+
+                На адрес email, указанный при регистрации, выслано письмо для подтверждения вашего адреса email. Это
+                процедура необходима для дальнейшей работе в системе. В настройках вашего профиля можно указать
+                рассылку уведомлений на события, происходящие в системе, которая будет посылаться на ваш адрес
+                email. Если вы не получили email, то
+                <form class="d-inline" method="post" action="{{ route('verification.resend') }}">
+                    @csrf
+                    <button type="submit"
+                            class="btn btn-link p-0 m-0 align-baseline">кликните сюда и система отправит вам новый
+                    </button>
+                    .
+                </form>
+            </div>
         </div>
     </div>
 </x-layout-auth>
