c0506d46a30f51979418f7fa8d1695795ad67ddf
Ian Travers <mr.itpe@gmail.com>
2020-10-02 09:23:51 +0300

    Install iziToast js library

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,6 +24,11 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
+ * iziToast
+ */
+window.iziToast = require('izitoast');
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
