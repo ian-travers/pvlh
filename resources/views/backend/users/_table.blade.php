61cec91876ea932940437ccda5fc458b24a3415b
Ian Travers <mr.itpe@gmail.com>
2020-10-08 11:38:33 +0300

    Install Fortawesome && using icons in the users' table (backend) && prepare quick buttons for further actions

--- resources/views/backend/users/_table.blade.php
+++ resources/views/backend/users/_table.blade.php
@@ -7,7 +7,8 @@
         <th>ФИО</th>
         <th>Должность</th>
         <th>Адрес email</th>
-        <th>Верификация</th>
+        <th class="text-center">Уведомления</th>
+        <th class="text-center">Верификация</th>
         <th class="text-center">Операции</th>
     </tr>
     </thead>
@@ -18,8 +19,45 @@
             <td>{{ $user->name }}</td>
             <td>{{ $user->position }}</td>
             <td>{{ $user->email }}</td>
-            <td>{{ $user->email_verified_at->diffForHumans() }}</td>
-            <td class="text-center"></td>
+            <td class="text-center">
+                <button
+                    type="button"
+                    class="btn btn-sm">
+                    <span
+                        class="fab fa-chrome {{ $user->hasBrowserNotifications() ? 'text-success' : 'text-secondary' }}"
+                    ></span>
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-sm">
+                    <span
+                        class="fa fa-at {{ $user->hasEmailNotifications() ? 'text-success' : 'text-secondary' }}"
+                    ></span>
+                </button>
+            </td>
+            <td class="text-center">{{ $user->email_verified_at ? $user->email_verified_at->diffForHumans() : '' }}</td>
+            <td class="text-center">
+                <button
+                    type="button"
+                    class="btn btn-sm btn-primary fa fa-user-edit"
+                    title="Редактировать">
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-sm btn-primary fa fa-user-check"
+                    title="Верифицировать">
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-sm btn-primary fa fa-unlock-alt"
+                    title="Сменить пароль">
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-sm btn-danger fa fa-trash-alt"
+                    title="Удалить">
+                </button>
+            </td>
         </tr>
     @endforeach
     </tbody>
