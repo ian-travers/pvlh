d56a15b78310bba31f7c93b4e741b5d97ebd2cdb
Ian Travers <mr.itpe@gmail.com>
2020-10-19 11:15:54 +0300

    Help section on the left sidebar

--- resources/views/components/backend-left-sidebar.blade.php
+++ resources/views/components/backend-left-sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-header text-white bg-primary">
-        Администрирование
+        <h4>Разделы системы</h4>
     </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item {{ $controller == 'PurposesController' ? 'active-nav-item' : '' }}">
@@ -11,3 +11,62 @@
         </li>
     </ul>
 </div>
+@if($controller != 'DashboardController')
+    <div class="card mt-3">
+        <div class="card-header text-dark bg-light">
+            <h4>Помощь по разделу</h4>
+        </div>
+        <div class="card-body">
+            @if($controller == 'PurposesController')
+                Этот раздел предназначен для создания, редактирования и удаления так называемых
+                <strong>назначений</strong>,
+                для которых планируется использование локомотива.
+                <br>
+                Для создания нового назначения&nbsp;&mdash; кнопка
+                <div class="text-center mt-1">
+                    <button type="button" class="btn btn-success">Создать</button>
+                </div>
+                <br>
+                Для редактирования или удаления назначения предназначены кнопки в колонке "Операции":
+                <div class="text-center mt-1">
+                    <button class="btn btn-sm btn-primary fa fa-edit mr-2" title="Редактировать"></button>
+                    <button class="btn btn-sm btn-danger fa fa-trash-alt" title="Удалить"></button>
+                </div>
+            @elseif($controller == 'UsersController')
+                Управление пользователями системы. Можно создавать новых пользователей. Кнопка:
+                <div class="text-center mt-1">
+                    <button class="btn btn-success">Создать</button>
+                </div>
+                К колонке "Операции" можно выполнить следующие действия:
+                <div>
+                    <button class="btn btn-sm btn-primary fa fa-user-edit" title="Редактировать"></button>&nbsp;&mdash;
+                    редактировать ФИО, должность, адрес email.
+                </div>
+                <div>
+                    <button class="btn btn-sm btn-primary fa fa-user-check" title="Верифицировать"></button>&nbsp;&mdash;
+                    верифицировать, т.е. подтвердить правильность адреса email. Может пригодиться для получения
+                    уведомлений.
+                </div>
+                <div>
+                    <button class="btn btn-sm btn-primary fa fa-unlock-alt" title="Сменить пароль"></button>&nbsp;&mdash;
+                    смена пароля пользователя.
+                </div>
+                <div>
+                    <button class="btn btn-sm btn-danger fa fa-trash-alt" title="Удалить"></button>&nbsp;&mdash;
+                    удаление пользователя.
+                </div>
+                <div class="mt-1">
+                    В колонке "Уведомления" можно управлять настройками уведомлений для верифицированных пользователей.
+                    Уведомления бывают 2-х видов:<br>
+                    <button class="btn p-1 fab fa-chrome text-success"></button>
+                    - в браузере<br>
+                    <button class="btn p-1 fa fa-at text-secondary"></button>
+                    - по электронной почте.
+                    Зеленый цвет означает, что уведомления включены. Серый - выключены. Клик по значку переключает
+                    состояние.
+                </div>
+            @endif
+        </div>
+    </div>
+@endif
+
